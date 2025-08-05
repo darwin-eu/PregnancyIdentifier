351c7235e139275f4877badd60621d69fbc5bdce
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2025-08-05 11:46:56 +0200

    added example script

---
+++ extras/CodeToRun.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(PregnancyIdentifier)
+
+cdmConnection <- DBI::dbConnect(
+  drv = RPostgres::Postgres(),
+  dbname = "...",
+  host = "...",
+  user = "...",
+  password = "..."
+)
+
+cdm <- CDMConnector::cdmFromCon(
+  con = cdmConnection,
+  cdmSchema = "...",
+  writeSchema = "..."
+)
+
+outputDir <- "./dev/output/"
+
+# runHip(cdm = cdm, outputDir = outputDir, continue = TRUE)
+# runPps(cdm = cdm, outputDir = outputDir)
+
+# ppsMinMax <- readRDS(file.path(outputDir, "PPS_min_max_episodes.rds"))
+# ppsEpisode <- readRDS(file.path(outputDir, "PPS_gest_timing_episodes.rds"))
+# hipRes <- read.csv("M:/Documents/hip_episodes.csv")
+
+# mergeHipPps(
+#   cdm = cdm,
+#   HIP = hipRes,
+#   PPSEpisode = ppsEpisode,
+#   PPSMinMax = ppsMinMax,
+#   outputDir = "./dev/output/",
+#   fileName = "merge.csv"
+# )
+
+cdm <- CDMConnector::readSourceTable(cdm = cdm, name = "initial_pregnant_cohort_df")
+
+hippsRes <- readRDS(file.path(outputDir, "HIPPS_episodes.rds"))
+
+debugonce(runEsd)
+runEsd(HIPPS = hippsRes, cdm = cdm, outputDir = outputDir)
