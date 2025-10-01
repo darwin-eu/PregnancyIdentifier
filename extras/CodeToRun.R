87f9431e0b2acb50e7898cbbd955bda57479761d
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2025-10-01 13:14:25 +0200

    Added codeToRun and exportResults.R

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -17,25 +17,24 @@ cdm <- CDMConnector::cdmFromCon(
 
 outputDir <- "./dev/output/"
 
-# runHip(cdm = cdm, outputDir = outputDir, continue = TRUE)
-# runPps(cdm = cdm, outputDir = outputDir)
+runHip(cdm = cdm, outputDir = outputDir, continue = TRUE)
+runPps(cdm = cdm, outputDir = outputDir)
 
-# ppsMinMax <- readRDS(file.path(outputDir, "PPS_min_max_episodes.rds"))
-# ppsEpisode <- readRDS(file.path(outputDir, "PPS_gest_timing_episodes.rds"))
-# hipRes <- read.csv("M:/Documents/hip_episodes.csv")
+ppsMinMax <- readRDS(file.path(outputDir, "PPS_min_max_episodes.rds"))
+ppsEpisode <- readRDS(file.path(outputDir, "PPS_gest_timing_episodes.rds"))
+hipRes <- read.csv(file.path(outputDir, "hip_episodes.csv"))
 
-# mergeHipPps(
-#   cdm = cdm,
-#   HIP = hipRes,
-#   PPSEpisode = ppsEpisode,
-#   PPSMinMax = ppsMinMax,
-#   outputDir = "./dev/output/",
-#   fileName = "merge.csv"
-# )
+mergeHipPps(
+  cdm = cdm,
+  HIP = hipRes,
+  PPSEpisode = ppsEpisode,
+  PPSMinMax = ppsMinMax,
+  outputDir = "./dev/output/",
+  fileName = "merge.csv"
+)
 
 cdm <- CDMConnector::readSourceTable(cdm = cdm, name = "initial_pregnant_cohort_df")
 
 hippsRes <- readRDS(file.path(outputDir, "HIPPS_episodes.rds"))
 
-debugonce(runEsd)
 runEsd(HIPPS = hippsRes, cdm = cdm, outputDir = outputDir)
