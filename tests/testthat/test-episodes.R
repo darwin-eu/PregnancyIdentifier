2a29f6e6d97f173acf74e6b9a12ff6c252c6bf3b
Ger Inberg <ginberg@gmail.com>
2025-12-02 14:22:46 +0100

    add test

---
+++ tests/testthat/test-episodes.R
@@ -0,0 +1,34 @@
+test_that("test gestational episodes", {
+  testPath <- getwd()
+  # Read patients from JSON
+  cdm <- TestGenerator::patientsCDM(
+    pathJson = testPath,
+    testName = "testData"
+  )
+
+  # start run analysis
+  outputDir <- file.path(testPath, "Results")
+  if (!dir.exists(outputDir)) {
+    dir.create(outputDir, recursive = T)
+  }
+  logger <- PregnancyIdentifier:::makeLogger(outputDir)
+
+  cdm <- PregnancyIdentifier:::uploadConceptSets(cdm, logger)
+
+  # insert gestation_visits data
+  gestation_visits <- read.csv("gestation_visits.csv", sep = ";") %>%
+    dplyr::mutate(visit_date = as.Date(visit_date),
+                  date_of_birth = as.Date(date_of_birth))
+  cdm <- omopgenerics::insertTable(cdm,
+                                   name = "gestation_visits_df",
+                                   table = gestation_visits,
+                                   overwrite = TRUE,
+                                   temporary = FALSE)
+
+  cdm <- PregnancyIdentifier:::gestation_episodes(cdm)
+  episodes <- dplyr::collect(cdm$gestation_episodes_df)
+
+  testthat::expect_equal(length(unique(episodes$episode)), 2)
+
+  unlink(outputDir, recursive = TRUE)
+})
