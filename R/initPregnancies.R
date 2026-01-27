8006bb144f5d82994865bbfc09d46976a5afb5b5
ablack3 <ablack3@users.noreply.github.com>
2026-01-27 18:54:04 +0000

    update initPregnancies

--- R/initPregnancies.R
+++ R/initPregnancies.R
@@ -40,132 +40,118 @@
 #' cdm <- mockPregnancyCdm()
 #' cdm <- initPregnancies(cdm)
 #' }
-initPregnancies <- function(cdm, startDate = as.Date("1900-01-01"), endDate = Sys.Date(), ageBounds = c(15L, 56L), logger = NULL) {
+initPregnancies <- function(cdm,
+                            startDate  = as.Date("1900-01-01"),
+                            endDate    = Sys.Date(),
+                            ageBounds  = c(15L, 56L),
+                            logger     = NULL) {
 
   checkmate::assertClass(cdm, "cdm_reference")
   checkmate::assertDate(startDate, any.missing = FALSE)
   checkmate::assertDate(endDate, any.missing = FALSE)
   checkmate::assertIntegerish(ageBounds, len = 2, any.missing = FALSE)
-  lowerAgeBound <- as.integer(ageBounds[1])
-  upperAgeBound <- as.integer(ageBounds[2]) + 1L
-  if (upperAgeBound < lowerAgeBound) {
-    rlang::abort("The lower age bound (ageBounds[1]) must be less than the upper age bound (ageBounds[2])")
-  }
   checkmate::assertClass(logger, "logger", null.ok = TRUE)
 
-  logInfo(logger, "Inserting HIP concepts into the CDM")
-  hipConcepts <- readxl::read_excel(system.file(package = "PregnancyIdentifier", "concepts", "HIP_concepts.xlsx"))
-  cdm <- CDMConnector::insertTable(cdm = cdm, name = "preg_hip_concepts", table = hipConcepts, overwrite = TRUE)
+  lowerAge <- as.integer(ageBounds[1])
+  upperAge <- as.integer(ageBounds[2]) + 1L
+  if (upperAge < lowerAge) {
+    rlang::abort("ageBounds[1] must be < ageBounds[2]")
+  }
 
-  logInfo(logger, "Inserting PPS concepts into the CDM")
+  logInfo(logger, "Loading + inserting pregnancy concept tables into the CDM")
+
+  hipConcepts <- readxl::read_excel(
+    system.file("concepts", "HIP_concepts.xlsx", package = "PregnancyIdentifier")
+  )
   ppsConcepts <- readxl::read_excel(
     system.file("concepts", "PPS_concepts.xlsx", package = "PregnancyIdentifier")
-  ) %>%
-    dplyr::rename_with(tolower) %>%
+  ) |>
+    dplyr::rename_with(tolower) |>
     dplyr::mutate(pps_concept_id = as.integer(.data$pps_concept_id))
 
-  cdm <- CDMConnector::insertTable(
-    cdm = cdm,
-    name = "preg_pps_concepts",
-    table = ppsConcepts
+  matchoTermDurations <- readxl::read_excel(
+    system.file("concepts", "Matcho_term_durations.xlsx", package = "PregnancyIdentifier")
   )
 
-  logInfo(logger, "Inserting Matcho term durations into the CDM")
-  matchoTermDurations <- readxl::read_excel(system.file(package = "PregnancyIdentifier", "concepts", "Matcho_term_durations.xlsx"))
-  cdm <- CDMConnector::insertTable(
-    cdm = cdm,
-    name = "preg_matcho_term_durations",
-    table = matchoTermDurations,
+  cdm <- CDMConnector::insertTable(cdm, "preg_hip_concepts", hipConcepts, overwrite = TRUE)
+  cdm <- CDMConnector::insertTable(cdm, "preg_pps_concepts", ppsConcepts, overwrite = TRUE)
+  cdm <- CDMConnector::insertTable(cdm, "preg_matcho_term_durations", matchoTermDurations, overwrite = TRUE)
+
+  # ---- helper: pull person_id + date + concept_id from an OMOP domain ----
+  pullDomain <- function(tbl, concept_col, date_col, extra = list()) {
+    tbl |>
+      dplyr::filter(.data[[date_col]] >= startDate, .data[[date_col]] <= endDate) |>
+      dplyr::transmute(
+        person_id   = .data$person_id,
+        concept_id  = .data[[concept_col]],
+        event_date  = .data[[date_col]],
+        !!!extra
+      )
+  }
+
+  # ============================================================
+  # HIP records (condition/procedure/observation/measurement)
+  # ============================================================
+  logInfo(logger, "Pulling HIP concept records from OMOP domain tables")
+
+  hip <- cdm$preg_hip_concepts |>
+    dplyr::select(concept_id, category)
+
+  hipSpecs <- tibble::tribble(
+    ~tbl,                         ~concept_col,               ~date_col,               ~value_col,
+    cdm$condition_occurrence,     "condition_concept_id",     "condition_start_date",  NA_character_,
+    cdm$procedure_occurrence,     "procedure_concept_id",     "procedure_date",        NA_character_,
+    cdm$observation,              "observation_concept_id",   "observation_date",      NA_character_,
+    cdm$measurement,              "measurement_concept_id",   "measurement_date",      "value_as_number"
   )
 
-  logInfo(logger, "Getting all initial pregnancy records")
-  hip <- dplyr::select(cdm$preg_hip_concepts, concept_id, category)
-
-  cdm$all_pregnancy_records <-
-    purrr::pmap(
-      list(
-        tbl     = list(cdm$measurement, cdm$procedure_occurrence, cdm$observation, cdm$condition_occurrence),
-        concept = c("measurement_concept_id","procedure_concept_id","observation_concept_id","condition_concept_id"),
-        date    = c("measurement_date","procedure_date","observation_date","condition_start_date")
-      ),
-      function (tbl, concept, date) {
-        valueExpr <- if (concept == "measurement_concept_id") {
-          rlang::sym("value_as_number")
-        } else {
-          rlang::expr(dplyr::sql("NULL"))
-        }
-
-        tbl %>%
-          dplyr::inner_join(hip, by = stats::setNames("concept_id", concept)) %>%
-          dplyr::filter(!!rlang::sym(date) >= startDate, !!rlang::sym(date) <= endDate) %>%
-          dplyr::transmute(
-            person_id = .data$person_id,
-            concept_id  = !!rlang::sym(concept),
-            visit_date  = !!rlang::sym(date),
-            category = .data$category,
-            value_as_number = !!valueExpr
-          ) %>%
-          dplyr::left_join(dplyr::select(cdm$concept, "concept_id", "concept_name"), by = "concept_id")
-      }
-    ) %>%
-    purrr::reduce(dplyr::union_all) %>%
+  hipEvents <- purrr::pmap(hipSpecs, function(tbl, concept_col, date_col, value_col) {
+    extra <- if (!is.na(value_col)) list(value_as_number = rlang::sym(value_col)) else list(value_as_number = NA_real_)
+    pullDomain(tbl, concept_col, date_col, extra)
+  }) |>
+    purrr::reduce(dplyr::union_all) |>
+    dplyr::inner_join(hip, by = "concept_id") |>
+    dplyr::left_join(dplyr::select(cdm$concept, concept_id, concept_name), by = "concept_id") |>
+    dplyr::rename(visit_date = event_date) |>
     dplyr::compute()
 
-  # keep only person_ids of women of reproductive age at some visit
-  cdm$preg_hip_records <- cdm$all_pregnancy_records %>%
-    addAgeSex("visit_date") %>%
-    dplyr::filter(.data$age >= lowerAgeBound, .data$age < upperAgeBound, .data$sex == "female") %>%
-    dplyr::distinct() %>%
-    dplyr::select("person_id", "visit_date", "category", "concept_id", "value_as_number") %>%
+  cdm$preg_hip_records <- hipEvents |>
+    addAgeSex("visit_date") |>
+    dplyr::filter(.data$sex == "female", .data$age >= lowerAge, .data$age < upperAge) |>
+    dplyr::distinct(person_id, visit_date, category, concept_id, value_as_number) |>
     dplyr::compute(name = "preg_hip_records", temporary = FALSE, overwrite = TRUE)
 
   # ============================================================
-  # Pull PPS concepts from OMOP domain tables
+  # PPS records (condition/procedure/measurement)
   # ============================================================
+  logInfo(logger, "Pulling PPS concept records from OMOP domain tables")
 
-  # Pulls concepts from a single OMOP table and normalizes column names so that
-  # all downstream logic can operate on a consistent schema.
-  # PPS concepts (in the PPS_concepts excel file) occur in the condition, measurement, and procedure domains
-  logInfo(logger, "Pulling PPS concept records")
-  domainSpecs <- tibble::tribble(
-    ~table_name,            ~date_column,            ~concept_id_column,
-    "condition_occurrence", "condition_start_date",  "condition_concept_id",
-    "procedure_occurrence", "procedure_date",        "procedure_concept_id",
-    "measurement",          "measurement_date",      "measurement_concept_id",
+  ppsSpecs <- tibble::tribble(
+    ~tbl,                     ~concept_col,              ~date_col,
+    cdm$condition_occurrence, "condition_concept_id",    "condition_start_date",
+    cdm$procedure_occurrence, "procedure_concept_id",    "procedure_date",
+    cdm$measurement,          "measurement_concept_id",  "measurement_date"
   )
 
-  cdm$all_timing_records <- purrr::map(seq_len(nrow(domainSpecs)), \(i) {
-    spec <- domainSpecs[i, ]
-
-    tableName <- spec$table_name[[1]]
-    dateCol   <- spec$date_column[[1]]
-    cidCol    <- spec$concept_id_column[[1]]
-
-    logInfo(logger, sprintf("Pulling data from %s", tableName))
-
-    cdm[[tableName]] %>%
-      dplyr::filter(.data[[dateCol]] >= startDate, .data[[dateCol]] <= endDate) %>%
+  ppsEvents <- purrr::pmap(ppsSpecs, function(tbl, concept_col, date_col) {
+    pullDomain(tbl, concept_col, date_col) |>
       dplyr::transmute(
         person_id,
-        pps_concept_start_date = .data[[dateCol]],
-        pps_concept_id         = .data[[cidCol]]
-      ) %>%
-      dplyr::inner_join(cdm$preg_pps_concepts, by = "pps_concept_id") %>%
-      dplyr::distinct()
-  }) %>% purrr::reduce(dplyr::union_all) %>%
+        pps_concept_start_date = event_date,
+        pps_concept_id         = concept_id
+      )
+  }) |>
+    purrr::reduce(dplyr::union_all) |>
+    dplyr::inner_join(cdm$preg_pps_concepts, by = "pps_concept_id") |>
+    dplyr::distinct() |>
     dplyr::compute()
 
-  cdm$preg_pps_records <- cdm$all_timing_records %>%
-    dplyr::filter(!is.na(.data$pps_concept_start_date)) %>%
-    addAgeSex("pps_concept_start_date") %>%
-    dplyr::filter(.data$sex == "female", .data$age >= 15, .data$age < 56) %>%
-    dplyr::select(-"sex", -"age") %>%
-    dplyr::compute(name = "preg_pps_records")
+  cdm$preg_pps_records <- ppsEvents |>
+    dplyr::filter(!is.na(.data$pps_concept_start_date)) |>
+    addAgeSex("pps_concept_start_date") |>
+    dplyr::filter(.data$sex == "female", .data$age >= lowerAge, .data$age < upperAge) |>
+    dplyr::select(-"sex", -"age") |>
+    dplyr::compute(name = "preg_pps_records", temporary = FALSE, overwrite = TRUE)
 
-  cdm <- omopgenerics::dropSourceTable(
-    cdm,
-    c("all_pregnancy_records", "all_timing_records")
-  )
-
-  return(cdm)
+  cdm
 }
