a4d5bfb581e4c5a8a41b2bdb08480c95751476f6
ablack3 <ablack3@users.noreply.github.com>
2026-01-31 02:18:23 +0000

    update tests

--- tests/testthat/test-episodes.R
+++ tests/testthat/test-episodes.R
@@ -1,35 +1,44 @@
-# test_that("test gestational episodes", {
-#   testPath <- system.file("testCases", mustWork = TRUE, package = "PregnancyIdentifier")
-#   # Read patients from JSON
-#   cdm <- TestGenerator::patientsCDM(
-#     pathJson = testPath,
-#     testName = "testData"
-#   )
-#
-#   # start run analysis
-#   outputDir <- file.path(tempdir(), "Results")
-#   if (!dir.exists(outputDir)) {
-#     dir.create(outputDir, recursive = T)
-#   }
-#   logger <- PregnancyIdentifier:::makeLogger(outputDir)
-#
-#   cdm <- initPregnancies(cdm)
-#
-#   # insert gestation_visits data
-#   gestation_visits <- utils::read.csv("gestation_visits.csv", sep = ";") %>%
-#     dplyr::mutate(visit_date = as.Date(visit_date),
-#                   date_of_birth = as.Date(date_of_birth))
-#   cdm <- omopgenerics::insertTable(cdm,
-#                                    name = "gestation_visits_df",
-#                                    table = gestation_visits,
-#                                    overwrite = TRUE,
-#                                    temporary = FALSE)
-#
-#   cdm <- PregnancyIdentifier:::gestationEpisodes(cdm, minDays = 70, bufferDays = 28)
-#   episodes <- dplyr::collect(cdm$gestation_episodes_df)
-#
-#   testthat::expect_equal(length(unique(episodes$episode)), 3)
-#
-#   unlink(outputDir, recursive = TRUE)
-#   cleanupCdmDb(cdm)
-# })
+# CSV contents (gestation_visits.csv): person_id;concept_id;visit_date;value_as_number;concept_name;category;gest_value;date_of_birth;date_diff;age
+# 12 rows for person_id=1: gestation visits 2021-05-27 through 2022-01-10 (episode 1), 2023-06-26 through 2024-04-20 (episodes 2 and 3).
+# buildGestationEpisodes(minDays=70, bufferDays=28) groups these into 3 distinct episodes.
+
+test_that("gestational episodes from preg_hip_records", {
+  cdm <- mockPregnancyCdm()
+  expect_s3_class(cdm, "cdm_reference")
+
+  outputDir <- file.path(tempdir(), "test_episodes")
+  dir.create(outputDir, recursive = TRUE, showWarnings = FALSE)
+  logger <- PregnancyIdentifier:::makeLogger(outputDir)
+
+  cdm <- initPregnancies(cdm)
+
+  # Overwrite preg_hip_records with custom gestation visits (columns required by buildGestationEpisodes)
+  gestation_visits <- utils::read.csv(
+    testthat::test_path("gestation_visits.csv"),
+    sep = ";"
+  ) %>%
+    dplyr::mutate(
+      visit_date = as.Date(.data$visit_date)
+    ) %>%
+    dplyr::select("person_id", "concept_id", "visit_date", "value_as_number", "category")
+
+  cdm <- CDMConnector::insertTable(
+    cdm,
+    name = "preg_hip_records",
+    table = gestation_visits,
+    overwrite = TRUE
+  )
+
+  cdm <- PregnancyIdentifier:::buildGestationEpisodes(
+    cdm,
+    logger = logger,
+    minDays = 70,
+    bufferDays = 28
+  )
+
+  episodes <- dplyr::collect(cdm$gest_episodes_df)
+  testthat::expect_equal(length(unique(episodes$episode)), 3L)
+
+  unlink(outputDir, recursive = TRUE)
+  cleanupCdmDb(cdm)
+})
